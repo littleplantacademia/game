eb40b6175865853d201c2363bf3e27adba774e34
littleplantacademia <134750563+littleplantacademia@users.noreply.github.com>
2023-05-28 22:15:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
   function showTextBox() {
   document.getElementById("textbox").style.display = "block";
 }
-
 function hideTextBox() {
   document.getElementById("textbox").style.display = "none";
 }
+const subTitleText = "Greetings and welcome to Little Plant Academia! Please choose your character.";
+
+let typewriter = document.getElementById("typewriter");
+let subTitleElement = typewriter.querySelector("p");
+let subTitleIndex = 0;
+
+function typeText() {
+  if (subTitleIndex < subTitleText.length) {
+    subTitleElement.textContent += subTitleText.charAt(subTitleIndex);
+    subTitleIndex++;
+  } else {
+    return; // Stop the typing effect when all text is displayed
+  }
+  
+  setTimeout(typeText, 50); // Adjust typing speed as desired
+}
+
+typeText();
+
 function showBox(boxId) {
       var boxes = document.getElementsByClassName("box");
       for (var i = 0; i < boxes.length; i++) {
